45fb515366703abf552d30ec51bc04ccd47ccc69
dave1780 <120386755+dave1780@users.noreply.github.com>
2023-03-23 18:58:55 +0545

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from "react";
 import "./style.css";
 import Header from "./Header.js"
+import useState from "react"
 export default function App() {
+  const [style,setStyle]=useState({
+    color:"red"
+  });
+  const colorHand=()=>{
+    setStyle({
+      color:"blue"
+    })
+  }
   return (
     <div>
-      <Header name={"sandeep"}/>
-      <h1>Hello StackBlitz!</h1>
+      <Header name={"we  got"}/>
+      <h1  styles={style} onClick={colorHand}>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <br/>
+      {
+         
+      }
+      
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,11 @@ const Header=(props)=>{
    const name=props.name
    if(name){
      return(
-       <h1>hello {name}</h1>
+       <h1>Hello! {name} </h1>
+     )
+   }else{
+     return (
+       <h1>Hello!</h1>
      )
    }
  }
